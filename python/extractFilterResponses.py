72c58521844bb1c75cc91cef3c0cba25a4a8544d
hamadkhan <hamad.khan@slu.edu>
2021-05-02 20:49:06 -0500

    Q1, Q2, and part of Q3

--- python/extractFilterResponses.py
+++ python/extractFilterResponses.py
@@ -1,46 +1,108 @@
 import cv2
 import numpy as np
+from random import randint
 from utils import *
 from createFilterBank import create_filterbank
 
 
-def extract_filter_responses (img, filterBank):
+def extract_filter_responses(img, filterBank):
 
     if len(img.shape) == 2:
-        img = cv2.merge ([img, img, img])
+        img = cv2.merge([img, img, img])
 
-    img = cv2.cvtColor (img, cv2.COLOR_BGR2Lab)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2Lab)
 
     # -----fill in your implementation here --------
-    filterResponses = np.ndarray((img.shape[0], img.shape[1], img.shape[2]*20))
-    # filterResponses[0, 0, 0] = (img, -1, filterBank[0])
-    for i in range(len(filterBank)):
-        filter = cv2.filter2D(img, -1, filterBank[i])
-        for x in range(img.shape[0]):
-            for y in range(img.shape[1]):
-                for z in range(3):
-                    filterResponses[x, y, 3*i+z] = filter[x, y, z]
+    filterResponses = []
+
+    # Lightness
+    light = img[:,:,0]
+
+    # Red/Green
+    redGreen = img[:,:,1]
+
+    # Blue/Green
+    blueGreen = img[:,:,2]
+
+    colorSpaces = [light, redGreen, blueGreen]
+    for colorSpace in colorSpaces:
+        for j in range(len(filterBank)):
+            dst = cv2.filter2D(colorSpace, -1, filterBank[j])
+            filterResponses.append(dst)
     # ----------------------------------------------
 
     return filterResponses
 
+def randPts(img, alpha):
+    """ Get random points """
+    ret = []
+    for entry in range(alpha):
+        xRand = randint(0, img.shape[1])
+        yRand = randint(0, img.shape[0])
+        ret.append([xRand, yRand])
+    return ret
+
+def getKps(img, alpha, k = 0.04):
+    """ Get top alpha keypoints """
+    # Get keypoints
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray = np.float32(gray)
+    kps = cv2.cornerHarris(gray, 2, 5, k)
+
+    # Make value array, which consists of [val, x, y]
+    valueArr = []
+    for row in range(kps.shape[0]):
+        for col in range(kps.shape[1]):
+            valueArr.append([kps[row][col], row, col])
+
+    # Shitty code
+    sortedValues = sorted(valueArr, key = lambda x: x[0], reverse = True) # Sort the keypoints in descending order
+    sortedValues = [elems[1:] for elems in sortedValues]
+    top50 = sortedValues[:alpha]
+    return top50
+
+def getDict(imgPaths, alpha, K, method):
+    fb = create_filterbank()
+    for fp in imgPaths:
+        # For each image, get the filter responses?
+        img = cv2.imread(fp)
+        responses = extract_filter_responses(img, fb)
+
+        # Get alpha points
+        if method == "Random":
+            pts = randPts(img, alpha)
+        elif method == "Harris":
+            pts = getKps(img, alpha)
+        
+        
+
+    return
+
 # start of some code for testing extract_filter_responses()
 if __name__ == "__main__":
-    fb = create_filterbank ()
-    img = cv2.imread ("../data/bedroom/sun_aiydcpbgjhphuafw.jpg")
+    fb = create_filterbank()
+    img = cv2.imread("../data/bedroom/sun_aiydcpbgjhphuafw.jpg")
 
-#    gray = cv2.cvtColor (img, cv2.COLOR_BGR2GRAY)
-#    print (extract_filter_responses (gray, fb))
+    #gray = cv2.cvtColor (img, cv2.COLOR_BGR2GRAY)
+    #print (extract_filter_responses (gray, fb))
 
-    responses = extract_filter_responses (img, fb)
+    #responses = extract_filter_responses(img, fb)
 
-    print(img.shape)
-    print(responses[0].shape)
+    #randomPts = randPts(img, 5)
+    #print(randomPts)
 
-    cv2.imshow('something',responses[0])
+    topKps = getKps(img, 5, 0.05)
+    print(topKps)
+    
+    #cv2.imshow('1',responses[0])
+    #cv2.imshow('2',responses[20])
+    #cv2.imshow('3',responses[40])
+
+    #cv2.imshow("merged", cv2.merge([responses[0], responses[20], responses[40]]))
 
     cv2.waitKey(0)
 
     cv2.destroyAllWindows()
+
 
 
