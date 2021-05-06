e1df9f999ed30f646a6bba9287b199878d17ec03
Anh Nguyen <anh.nguyen@slu.edu>
2021-05-05 20:55:57 -0500

    Progress on Q2.1.

--- python/getVisualWords.py
+++ python/getVisualWords.py
@@ -6,7 +6,13 @@ from extractFilterResponses import extract_filter_responses
 def get_visual_words (img, dictionary, filterBank):
 
     # -----fill in your implementation here --------
+    wordMap = np.zeros ((img.shape[0], img.shape[1]))
+    filterResponses = extract_filter_responses(img, filterBank)
 
+    for x in range(len(img.shape[0])):
+        for y in range(len(img.shape[1])):
+            distances = cdist(img[x,y], wordMap)
+            minDistance = min(distances)
 
     # ----------------------------------------------
 
