f23f888121059609e31f597b4b91c670d4d6b706
Anh Nguyen <anh.nguyen@slu.edu>
2021-05-06 20:26:29 -0500

    More progress on Q2.1.

--- python/getVisualWords.py
+++ python/getVisualWords.py
@@ -1,6 +1,9 @@
 import numpy as np
 from scipy.spatial.distance import cdist
 from extractFilterResponses import extract_filter_responses
+import pickle
+from createFilterBank import create_filterbank
+import cv2
 
 
 def get_visual_words (img, dictionary, filterBank):
@@ -8,10 +11,11 @@ def get_visual_words (img, dictionary, filterBank):
     # -----fill in your implementation here --------
     wordMap = np.zeros ((img.shape[0], img.shape[1]))
     filterResponses = extract_filter_responses(img, filterBank)
+    responses2D = np.zeros ((m*n, 3*len(filterBank)))
 
-    for x in range(len(img.shape[0])):
-        for y in range(len(img.shape[1])):
-            distances = cdist(img[x,y], dictionary)
+    for x in range(img.shape[0]):
+        for y in range(img.shape[1]):
+            distances = cdist(responses2D, dictionary)
             minDistance = min(distances)
             index = np.where(distances == minDistance)
             label = np.where(dictionary[index])
@@ -23,5 +27,12 @@ def get_visual_words (img, dictionary, filterBank):
 
     return wordMap
 
+#Test
 if __name__ == '__main__':
+    img = cv2.imread('../data/airport/sun_aesovualhburmfhn.jpg')
+    dict = pickle.load(open( "dictionaryHarris.pkl", "rb" ))
+    filterBank = create_filterbank()
 
+    wordMap = get_visual_words(img, dict, filterBank)
+
+    print(wordMap)
