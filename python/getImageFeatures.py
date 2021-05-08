58bcfff167242a8d26b7cd6315e5302c7cc28821
Anh Nguyen <anh.nguyen@slu.edu>
2021-05-08 13:25:19 -0500

    Q2.2.

--- python/getImageFeatures.py
+++ python/getImageFeatures.py
@@ -4,7 +4,15 @@ import numpy as np
 def get_image_features (wordMap, dictionarySize):
 
     # -----fill in your implementation here --------
+    h = np.zeros ((1, dictionarySize))
 
+    for i in range(len(wordMap.shape[0])):
+        for j in range(len(wordMap.shape[1])):
+            for k in range(dictionarySize):
+                if wordMap[i,j]==k:
+                    h[k]=h[k]+1
+
+    h = h/np.sum(h)
 
     # ----------------------------------------------
     
--- python/getVisualWords.py
+++ python/getVisualWords.py
@@ -11,16 +11,19 @@ def get_visual_words (img, dictionary, filterBank):
     # -----fill in your implementation here --------
     wordMap = np.zeros ((img.shape[0], img.shape[1]))
     filterResponses = extract_filter_responses(img, filterBank)
-    responses2D = np.zeros ((m*n, 3*len(filterBank)))
+    # responses2D = np.zeros ((m*n, 3*len(filterBank)))
 
-    for x in range(img.shape[0]):
-        for y in range(img.shape[1]):
-            distances = cdist(responses2D, dictionary)
-            minDistance = min(distances)
-            index = np.where(distances == minDistance)
-            label = np.where(dictionary[index])
+    # print(filterResponses[0, 0, 0])
+    # print("Dictionary\n", dictionary)
+    for x in range(filterResponses.shape[0]):
+    #     for y in range(filterResponses.shape[1]):
+    #         for z in range(filterResponses.shape[2]):
+        distances = cdist(filterResponses[x], dictionary)
+        minDistance = np.amin(distances)
+        index = np.where(distances == minDistance)
+        # label = np.where(dictionary[index])
 
-            wordMap[x, y] = label
+        # wordMap[x, y] = label
 
 
     # ----------------------------------------------
