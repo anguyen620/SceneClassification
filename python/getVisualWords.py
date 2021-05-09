9273f214fca0a58a15ceaeaaa1f499aecc4421bb
EricHusa <ericrhusa@gmail.com>
2021-05-09 11:38:36 -0500

    Add changes for Q2 from other branch

--- python/getVisualWords.py
+++ python/getVisualWords.py
@@ -4,6 +4,7 @@ from extractFilterResponses import extract_filter_responses
 import pickle
 from createFilterBank import create_filterbank
 import cv2
+from skimage.color import label2rgb
 
 
 def get_visual_words (img, dictionary, filterBank):
@@ -11,20 +12,11 @@ def get_visual_words (img, dictionary, filterBank):
     # -----fill in your implementation here --------
     wordMap = np.zeros ((img.shape[0], img.shape[1]))
     filterResponses = extract_filter_responses(img, filterBank)
-    # responses2D = np.zeros ((m*n, 3*len(filterBank)))
 
-    # print(filterResponses[0, 0, 0])
-    # print("Dictionary\n", dictionary)
-    for x in range(filterResponses.shape[0]):
-    #     for y in range(filterResponses.shape[1]):
-    #         for z in range(filterResponses.shape[2]):
-        distances = cdist(filterResponses[x], dictionary)
-        minDistance = np.amin(distances)
-        index = np.where(distances == minDistance)
-        # label = np.where(dictionary[index])
-
-        # wordMap[x, y] = label
-
+    for x in range(img.shape[0]):
+        for y in range(img.shape[1]):
+            distances = cdist(dictionary, filterResponses[x][y].reshape(1,60))
+            wordMap[x, y] = np.argmin(distances)
 
     # ----------------------------------------------
 
@@ -33,9 +25,22 @@ def get_visual_words (img, dictionary, filterBank):
 #Test
 if __name__ == '__main__':
     img = cv2.imread('../data/airport/sun_aesovualhburmfhn.jpg')
-    dict = pickle.load(open( "dictionaryHarris.pkl", "rb" ))
+    rand_dict = pickle.load(open( "dictionaryRandom.pkl", "rb" ))
+    harris_dict = pickle.load(open( "dictionaryHarris.pkl", "rb" ))
     filterBank = create_filterbank()
 
-    wordMap = get_visual_words(img, dict, filterBank)
+    randomWordMap = get_visual_words(img, rand_dict, filterBank)
+    harrisWordMap = get_visual_words(img, harris_dict, filterBank)
 
-    print(wordMap)
+    visualize_random = label2rgb(randomWordMap, img, bg_label=0)
+    visualize_harris = label2rgb(harrisWordMap, img, bg_label=0)
+    cv2.imshow(str("Random Word Map in RGB"), visualize_random)
+    cv2.imshow(str("Harris Word Mapp in RGB"), visualize_harris)
+
+    visualize_random = cv2.cvtColor(np.float32(visualize_random), cv2.COLOR_RGB2BGR)
+    visualize_harris = cv2.cvtColor(np.float32(visualize_harris), cv2.COLOR_RGB2BGR)
+    cv2.imshow(str("Random Word Map in BGR?"), visualize_random)
+    cv2.imshow(str("Harris Word Mapp in BGR?"), visualize_harris)
+
+    cv2.waitKey(0)
+    cv2. destroyAllWindows()
